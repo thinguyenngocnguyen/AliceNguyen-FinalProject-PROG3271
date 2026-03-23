121905c01ab09bf501edaef3c6c4f45a173026f8
Alice Nguyen <alicenguye.ca06@gmail.com>
2026-03-23 10:33:44 -0400

    server active

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response } from 'express'; 
+//import ConnectToDb from './infrastructure/connection';
+//import { config } from "./config/config"
 
 const app = express()
-const port = 3000
+const port = 3000;
+
+//Connect to the database
+//ConnectToDb();
+
+app.use(express.json()) //middleware to parse incoming JSON requests
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Online forum backend is running')
@@ -10,3 +17,8 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
+// import express from 'express';
+
+// const app = express(); //create an express app 
+
+export default app;
