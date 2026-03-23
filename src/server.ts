61ca8da74f3ad2449a2fa2c235d723870164d0ca
Alice Nguyen <alicenguye.ca06@gmail.com>
2026-03-23 12:25:27 -0400

    call connect to DB

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response } from 'express'; 
-//import ConnectToDb from './infrastructure/connection';
-//import { config } from "./config/config"
+import connectToDatabase from './infrastructure/connection';
 import {PORT} from './config/config'
 import userRouter from './ports/routes/user.router';
 import postRouter from './ports/routes/post.router';
 import authRouter from './ports/routes/auth.routes';
 import commentRouter from './ports/routes/comment.router';
+import adminRouter from './ports/routes/admin.router';
 
 const app = express()
 const port = 3000;
@@ -20,13 +20,16 @@ app.use('/api/v1/users', userRouter);
 app.use('/api/v1/posts', postRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/comments', commentRouter);
+app.use('/api/v1/admin', adminRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Online forum backend is running')
 })
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
+app.listen(PORT, async () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+
+  await connectToDatabase();
 })
 
 export default app;
