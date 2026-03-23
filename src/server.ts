2c0ca20254ca47b1e3e214c1484eb9ff953fe203
Alice Nguyen <alicenguye.ca06@gmail.com>
2026-03-23 11:46:02 -0400

    import all routers into server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express, { Request, Response } from 'express';
 //import ConnectToDb from './infrastructure/connection';
 //import { config } from "./config/config"
 import {PORT} from './config/config'
+import userRouter from './ports/routes/user.router';
+import postRouter from './ports/routes/post.router';
+import authRouter from './ports/routes/auth.routes';
+import commentRouter from './ports/routes/comment.router';
 
 const app = express()
 const port = 3000;
@@ -10,6 +14,12 @@ const port = 3000;
 //ConnectToDb();
 
 app.use(express.json()) //middleware to parse incoming JSON requests
+
+// Routes
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/posts', postRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/comments', commentRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Online forum backend is running')
