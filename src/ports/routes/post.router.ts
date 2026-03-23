91da1e32d3f6478d96e3a14891b24ab051c08c87
Alice Nguyen <alicenguye.ca06@gmail.com>
2026-03-23 11:24:17 -0400

    update actions for posts

---
+++ src/ports/routes/post.router.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+const postRouter = Router();
+
+postRouter.get('/', (req, res) => res.send({title: 'GET all posts'}));
+postRouter.get('/:id', (req, res) => res.send({title: 'GET post by id'}));
+
+//Procted router (user must be logged in to create, update, delete posts and add comments)
+
+// add authentication middleware after////
+postRouter.post('/', (req, res) => res.send({title: 'Create a new post'}));
+postRouter.put('/:id', (req, res) => res.send({title: 'Update post by id'}));
+postRouter.delete('/:id', (req, res) => res.send({title: 'Delete post by id'}));
+postRouter.post('/:id/comments', (req, res) => res.send({title: 'Add a comment to a post'}));
+
+postRouter.post('/:id/like', (req, res) => res.send({title: 'Like a post'}));
+postRouter.post('/:id/unlike', (req, res) => res.send({title: 'Unlike a post'}));
+
+
+// update super user here////
+
+export default postRouter;
