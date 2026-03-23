7d47ff47ef48698eeaa731939df22d8a8032d6da
Alice Nguyen <alicenguye.ca06@gmail.com>
2026-03-23 11:51:40 -0400

    Admin router to display user analytics and overall site statistics

---
+++ src/ports/routes/admin.router.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const adminRouter = Router();
+
+// Get overall site statistics
+adminRouter.get('/stats', (req, res) => {res.send({ title: 'GET overall site statistics' });});
+// Get user analytics
+adminRouter.get('/analytics/users', (req, res) => {res.send({ title: 'GET user analytics' });});
+// Get post analytics
+adminRouter.get('/analytics/posts', (req, res) => {res.send({ title: 'GET post analytics' });});
+// Get comment analytics
+adminRouter.get('/analytics/comments', (req, res) => {res.send({ title: 'GET comment analytics' });});
+export default adminRouter;
