eaba34993ebae687b800d33252604603a375ef06
Alice Nguyen <alicenguye.ca06@gmail.com>
2026-03-23 12:24:14 -0400

    add a default route

--- src/ports/routes/admin.router.ts
+++ src/ports/routes/admin.router.ts
@@ -2,6 +2,10 @@ import { Router } from "express";
 
 const adminRouter = Router();
 
+adminRouter.get('/', (req, res) => {
+    res.send({ title: 'Admin root endpoint' });
+});
+
 // Get overall site statistics
 adminRouter.get('/stats', (req, res) => {res.send({ title: 'GET overall site statistics' });});
 // Get user analytics
@@ -10,4 +14,5 @@ adminRouter.get('/analytics/users', (req, res) => {res.send({ title: 'GET user a
 adminRouter.get('/analytics/posts', (req, res) => {res.send({ title: 'GET post analytics' });});
 // Get comment analytics
 adminRouter.get('/analytics/comments', (req, res) => {res.send({ title: 'GET comment analytics' });});
+
 export default adminRouter;
