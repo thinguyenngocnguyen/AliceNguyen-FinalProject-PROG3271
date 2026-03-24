f9645e8c344a1beb01e8323b9f924e7e82f2fc44
Alice Nguyen <alicenguye.ca06@gmail.com>
2026-03-24 11:11:00 -0400

    Add data structure of user

---
+++ src/infrastructure/mongodb/models/user.model.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { match } from "node:assert";
+import { time } from "node:console";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: [true, 'Username is required'], unique: true },
+  email: { type: String, required: [true, 'Email is required'], unique: true, match: [/\S+@\S+\.\S+/, 'Please use a valid email address'] },
+  password: { type: String, required: [true, 'Password is required'], minLength: 6 },
+  role: { type: String, enum: ['user', 'super user','admin'], default: 'user' },
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
+
