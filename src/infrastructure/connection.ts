761431e69faf35d802f9ed21bb214cb986e629bf
Alice Nguyen <alicenguye.ca06@gmail.com>
2026-03-24 12:47:03 -0400

    remove unnecessary code lines

--- src/infrastructure/connection.ts
+++ src/infrastructure/connection.ts
@@ -1,20 +1,3 @@
-// import mongoose from 'mongoose';
-// import { config } from "../../../../config/config"
-// //import mongoose from '../../../../../node_modules/mongoose/types/index.d';
-
-// //const mongoDb = config.mongo.url;
-
-// export const ConnectToDb = async () => {
-//   try {
-//     await mongoose.connect(mongoDb);
-//     console.log('Connected to database successfully');
-//     // connection(mongoDb).then(() => {
-//     //   console.log('Connected to database successfully');
-//     // })
-//   } catch (err) {
-//     console.log('Disconnect', err);
-//   }
-// }
 
 import mongoose from 'mongoose';
 import { MONGODB_URI, NODE_ENV } from '../config/config';
