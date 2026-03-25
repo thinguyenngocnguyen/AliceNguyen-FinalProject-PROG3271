ae53e0c9cfb940d46c439222036683ef3d37d28e
Alice Nguyen <alicenguye.ca06@gmail.com>
2026-03-25 14:08:54 -0400

    Added createPost and authorize into router

--- src/ports/routes/post.router.ts
+++ src/ports/routes/post.router.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { createPost } from "../../controllers/post.controller";
+import authorize from "../middlewares/auth.middleware";
 
 const postRouter = Router();
 
@@ -8,12 +10,15 @@ postRouter.get('/:id', (req, res) => res.send({title: 'GET post by id'}));
 //Procted router (user must be logged in to create, update, delete posts and add comments)
 
 // add authentication middleware after////
-postRouter.post('/', (req, res) => res.send({title: 'Create a new post'}));
+postRouter.post('/create-post', authorize, createPost);
 postRouter.put('/:id', (req, res) => res.send({title: 'Update post by id'}));
 postRouter.delete('/:id', (req, res) => res.send({title: 'Delete post by id'}));
 
 //Likes
 postRouter.post('/:id/like', (req, res) => res.send({title: 'Like a post'}));
 postRouter.post('/:id/unlike', (req, res) => res.send({title: 'Unlike a post'}));
+
+//Super user force edit others post
+postRouter.put('/:postId/force-edit', (req, res) => res.send({title: 'Super user force edit others post'}));
 
 export default postRouter;
