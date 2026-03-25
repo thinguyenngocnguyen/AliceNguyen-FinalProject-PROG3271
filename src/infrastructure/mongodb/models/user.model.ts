afdfc498e7898b89ba102173fb171cdb55acfe41
Alice Nguyen <alicenguye.ca06@gmail.com>
2026-03-25 13:42:18 -0400

    add default role

--- src/infrastructure/mongodb/models/user.model.ts
+++ src/infrastructure/mongodb/models/user.model.ts
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: [true, 'Username is required'], unique: true },
   email: { type: String, required: [true, 'Email is required'], unique: true, match: [/\S+@\S+\.\S+/, 'Please use a valid email address'] },
   password: { type: String, required: [true, 'Password is required'], minLength: 6 },
-  role: { type: String, enum: ['user', 'super user','admin'], default: 'user' },
+  role: { type: String, enum: ['user', 'superuser','admin'], default: 'user' },
 }, {timestamps: true});
 
 const User = mongoose.model('User', userSchema);
