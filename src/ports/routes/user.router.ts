48c40edf2c3177b00d8f3f9df9247e3bb6eada07
Alice Nguyen <alicenguye.ca06@gmail.com>
2026-03-25 13:41:13 -0400

    call functions from user controller

--- src/ports/routes/user.router.ts
+++ src/ports/routes/user.router.ts
@@ -1,15 +1,20 @@
 import { Router } from "express";
+import { getAllUsers } from "../../controllers/user.controller";
+import { getUser } from "../../controllers/user.controller";
+import { createUser } from "../../controllers/user.controller";
+import { updateUser } from "../../controllers/user.controller";
+import authorize from "../middlewares/auth.middleware";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({title: 'GET all users'}));
-userRouter.get('/:id', (req, res) => res.send({title: 'GET user by id'}));
-userRouter.post('/', (req, res) => res.send({title: 'Create a new user'}));
-userRouter.put('/:id', (req, res) => res.send({title: 'Update user by id'}));
+userRouter.get('/', getAllUsers);
+userRouter.get('/:id', authorize,getUser);
+userRouter.post('/', createUser);
+userRouter.put('/:id', updateUser);
 userRouter.delete('/:id', (req, res) => res.send({title: 'Delete user by id'}));
 
 // update super user here////
 
-userRouter.put('/:id/role', (req, res) => res.send({title: 'Update user role: superuser'}));
-userRouter.put('/:userId/posts/:postId/force-edit', (req, res) => res.send({title: 'Super user force edit others post'}));
+userRouter.put('/:id/role', updateUser);
+
 export default userRouter;
