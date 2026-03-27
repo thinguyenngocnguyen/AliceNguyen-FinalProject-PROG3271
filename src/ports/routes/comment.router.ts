313359933e1b7689f064a795bdbcabe65508e0db
Alice Nguyen <alicenguye.ca06@gmail.com>
2026-03-27 02:31:12 -0400

    Add comment funtion into route

--- src/ports/routes/comment.router.ts
+++ src/ports/routes/comment.router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import authorize from '../middlewares/auth.middleware';
+import { createComment } from "../../controllers/comment.controller";
 
 const commentRouter = Router();
 
 commentRouter.get('/', (req, res) => res.send({title: 'GET all comments'}));
 commentRouter.get('/:id', (req, res) => res.send({title: 'GET comment by id'}));
-commentRouter.post('/', (req, res) => res.send({title: 'Create a new comment'}));
+commentRouter.post('/create-comment', authorize, createComment);
 commentRouter.put('/:id', (req, res) => res.send({title: 'Update comment by id'}));
 commentRouter.delete('/:id', (req, res) => res.send({title: 'Delete comment by id'}));
 
